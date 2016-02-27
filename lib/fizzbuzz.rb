89efb945a4dc0253d8d8801352cc622149bf80c3
MBP_roro <rodolphe.duterval@gmail.com>
2016-02-27 20:49:38 +0100

    initial commit

---
+++ lib/fizzbuzz.rb
@@ -0,0 +1,8 @@
+class Integer
+	def fizzbuzz
+		return "fizzbuzz" if self % 15 == 0
+		return "fizz" if self % 3      == 0
+		return "buzz" if self % 5      == 0
+		self
+	end
+end
---
+++ spec/fizzbuzz_spec.rb
@@ -0,0 +1,83 @@
+require "fizzbuzz"
+
+describe "fizzbuzz" do
+	it "return 1 when passed 1" do
+	expect(1.fizzbuzz).to eq 1
+	end
+	
+	it "return 2 when passed 2" do
+	expect(2.fizzbuzz).to eq 2
+	end
+	
+	it "return \"fizz\" when passed 3" do
+	expect(3.fizzbuzz).to eq "fizz"
+	end
+	
+	it "return 4 when passed 4" do
+	expect(4.fizzbuzz).to eq 4
+	end
+	
+	it "return \"buzz\" when passed 5" do
+	expect(5.fizzbuzz).to eq "buzz"
+	end
+
+	it "returns \"fizz\" when passed 6" do
+    expect(6.fizzbuzz).to eq "fizz"
+  end
+
+  it "returns 7 when passed 7" do
+    expect(7.fizzbuzz).to eq 7
+  end
+
+  it "returns 8 when passed 8" do
+    expect(8.fizzbuzz).to eq 8
+  end
+
+  it "returns \"fizz\" when passed 9" do
+    expect(9.fizzbuzz).to eq "fizz"
+  end
+
+  it "returns \"buzz\" when passed 10" do
+    expect(10.fizzbuzz).to eq "buzz"
+  end
+
+  it "returns 11 when passed 11" do
+    expect(11.fizzbuzz).to eq 11
+  end
+
+  it "returns \"fizz\" when passed 12" do
+    expect(12.fizzbuzz).to eq "fizz"
+  end
+
+  it "returns 13 when passed 13" do
+    expect(13.fizzbuzz).to eq 13
+  end
+
+  it "returns 14 when passed 14" do
+    expect(14.fizzbuzz).to eq 14
+  end
+
+  it "returns \"fizzbuzz\" when passed 15" do
+    expect(15.fizzbuzz).to eq "fizzbuzz"
+  end
+
+  it "returns 16 when passed 16" do
+    expect(16.fizzbuzz).to eq 16
+  end
+
+  it "returns 17 when passed 17" do
+    expect(17.fizzbuzz).to eq 17
+  end
+
+  it "returns \"fizz\" when passed 18" do
+    expect(18.fizzbuzz).to eq "fizz"
+  end
+
+  it "returns 19 when passed 19" do
+    expect(19.fizzbuzz).to eq 19
+  end
+
+  it "returns \"buzz\" when passed 20" do
+    expect(20.fizzbuzz).to eq "buzz"
+  end
+end
