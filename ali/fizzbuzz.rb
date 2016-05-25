c6b24217e309ab9f42671af8e4df615cafa7ea0b
syed zeeshan ali zaidi <zeshanalizaidi@gmail.com>
2016-05-25 17:23:25 +0200

    ali

---
+++ ali/fizzbuzz.rb
@@ -0,0 +1,19 @@
+class Integer
+   def fizzbuzz
+       1.upto(100) do |i|
+          if i % 5 == 0 and i % 3 == 0
+             puts "FizzBuzz"
+          elsif i % 5 == 0
+             puts "Buzz"
+          elsif i % 3 == 0
+             puts "Fizz"
+          else
+             puts i
+       end
+    end
+   end
+endputs 2.fizzbuzz
+puts 5.fizzbuzz
+puts 6.fizzbuzz
+puts 11.fizzbuzz
+puts 30.fizzbuzz
