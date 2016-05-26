17b85ff7e15fd1df74e5d0f390d5b832eaf4129d
thamoui <hamoui.antoine@gmail.com>
2016-05-26 10:58:15 +0200

    antoine

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,7 +1,13 @@
-class Integer
-   def fizzbuzz
-      #
-      # YOUR CODE GOES HERE
-      #
-   end
+def fizzbuzz(range, triggers)
+  range.each do |i|
+    result = ''
+    triggers.each do |(text, divisor)|
+      result << text if i % divisor == 0
+    end
+    puts result == '' ? i : result
+  end
 end
+fizzbuzz(1..100, [
+  ['Fizz', 3],
+  ['Buzz', 5],
+])
